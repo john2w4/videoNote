231b2abc2e4e6faf228267190127f5a4cc3642c0
john <nanchanqiri7@gmail.com>
2025-08-30 17:41:51 +0800

    Initial Commit

---
+++ VideoNote/VideoNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoNoteApp.swift
+//  VideoNote
+//
+//  Created by wh on 2025/8/30.
+//
+
+import SwiftUI
+
+@main
+struct VideoNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VideoNoteTests/VideoNoteTests.swift
@@ -0,0 +1,16 @@
+//
+//  VideoNoteTests.swift
+//  VideoNoteTests
+//
+//  Created by wh on 2025/8/30.
+//
+
+import Testing
+
+struct VideoNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
